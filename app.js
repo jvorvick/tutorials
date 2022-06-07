0c8da20805a1dd34e716f500eb9c43d61159cf3c
jvorvick <49791235+jvorvick@users.noreply.github.com>
2022-06-06 20:06:08 -0700

    typescript tutorial

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //   throw new Error('Incorrect input!');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: 'Jesse',
+    age: 31,
+    hobbies: ['Video Games', 'Martial Arts']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5; // 5.0
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
