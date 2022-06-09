513c37ca26faf1bdb462618c2e54358be2c87014
jvorvick <49791235+jvorvick@users.noreply.github.com>
2022-06-08 20:25:01 -0700

    typescript tutorial

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 var person = {
     name: 'Jesse',
     age: 31,
-    hobbies: ['Video Games', 'Martial Arts']
+    hobbies: ['Video Games', 'Martial Arts'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+// person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !! ERROR !!
 }
